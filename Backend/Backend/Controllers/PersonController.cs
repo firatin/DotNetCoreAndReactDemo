a2d419ce92273e005f51778e96a76b39a9c8cfd1
firatin <github@firatin.com>
2025-12-19 17:02:38 +0300

    Dotnet db operations .added -Sqlite API crud endpoints .added API testing service .added -REST Client

--- Backend/Backend/Controllers/PersonController.cs
+++ Backend/Backend/Controllers/PersonController.cs
@@ -1,5 +1,7 @@
+using backend.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend.Controllers;
 
@@ -7,5 +9,85 @@ namespace backend.Controllers;
 [ApiController]
 public class PersonController : ControllerBase
 {
+    private readonly AppDbContext _db;
+
+    public PersonController(AppDbContext context)
+    {
+        _db = context;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddPerson(Person person)
+    {
+        try
+        {
+            _db.Person.Add(person);
+            await _db.SaveChangesAsync();
+            return Ok(person);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+    [HttpGet]
+    public async Task<IActionResult> GetPersonList()
+    {
+        try
+        {
+            //var personList= await _db.Person.ToListAsync();
+            var personList = _db.Person.ToListAsync();           
+            return Ok(personList);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+    [HttpGet("{personID:int}")]
+    public async Task<IActionResult> GetPerson(int personID)
+    {
+        try
+        {
+            //var person = await _db.Person.FindAsync(personID);
+            var person = await _db.Person.Where(x => x.ID == personID).ToListAsync();
+            return Ok(person);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdatePerson(Person person)
+    {
+        try
+        {
+            _db.Person.Update(person);
+            await _db.SaveChangesAsync();
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpDelete("{personID:int}")]
+    public async Task<IActionResult> DeletePerson(int personID)
+    {
+        try
+        {
+            var person = await _db.Person.FindAsync(personID);
+              _db.Person.Remove(person);
+              await _db.SaveChangesAsync();
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
 }
 
